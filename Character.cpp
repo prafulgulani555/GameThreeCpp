a6c139185c4ceedf6e6faae7ae53e280cb8467da
Praful Gulani <prafulgulani555@gmail.com>
2024-04-06 13:34:45 +0530

    character on map

--- Character.cpp
+++ Character.cpp
@@ -1,8 +1,20 @@
-#include "raylib.h"
 #include "raymath.h"
 #include "Character.h"
 
-int main()
+Character::Character()
+{
+    width = texture.width / maxFrames;
+    height = texture.height;
+}
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {
+        (float)winWidth / 2.0f - 4.0f * (0.5f * width),
+        (float)winHeight / 2.0f - 4.0f * (0.5f * height)};
+}
+
+void Character::tick(float deltaTime)
 {
     Vector2 direction{};
     if (IsKeyDown(KEY_A))
@@ -39,36 +51,7 @@ int main()
     }
 
     // character drawing
-    Rectangle source{frame * (float)texture.width / 6.f, 0.f, rightLeft * (float)texture.width / 6.f, (float)texture.height};
-    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width / 6.0f, 4.0f * (float)texture.height};
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f * width, 4.0f * height};
     DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-}
-
-int main()
-{
-    const int windowWidth{384};
-    const int windowHeight{384};
-    InitWindow(windowWidth, windowHeight, "Top Down Game");
-
-    Texture2D map = LoadTexture("nature_tilesets/worldMap.png");
-    Vector2 mapPos{0.0, 0.0};
-   
-   Character knight;
-   knight.setScreenPos(windowWidth, windowHeight);
-
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(WHITE);
-
-        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
-
-        // map drawing
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
-        knight.tick(GetFrameTime());
-
-        EndDrawing();
-    }
-    CloseWindow();
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Top Down Game
+
+- This is my Third game using c++, raylib library and Tiled. 
+- This game is taught in [udemy's course](https://www.udemy.com/share/104VaU3@ZH05jPKOvVZiQD55K9vAAUrbHm3cZcqlgrwYDMlWMbxB5j0hWjN_jRIQ-nwcldfn/)
+- Map for this game was created using Tiled
+- Resources from [itch.io](https://itch.io/game-assets/free) were used in making of this game. 
+- [RPG Nature Tileset](https://stealthix.itch.io/rpg-nature-tileset?download) is used in this game which is available on [itch.io](https://itch.io/game-assets/free).
+- This is a Top Down Game.
